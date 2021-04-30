e4627d8d768feeb7eaee95ff522686f973f9bb0f
dimplehh <dimplehh@naver.com>
2021-04-30 23:44:59 +0900

    메뉴 전까지 html/css만듦

---
+++ template.js
@@ -0,0 +1,13 @@
+module.exports = {
+        html:
+        `
+        <!DOCTYPE html>
+        <html lang="ko">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Welcome to my Page!
+        </head>
+
+        `
+}
